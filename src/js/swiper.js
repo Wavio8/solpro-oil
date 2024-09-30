4cb35481d16b73e4b3b23fe6e0007c6011919386
Alena Berseneva <alen1313131313@mail.ru>
2024-09-30 12:22:40 +0300

    fix: add new section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -8,3 +8,29 @@ var swiper = new Swiper(".mySwiper", {
 });
 };
 export default swiperFn;
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Находим все элементы с классом .wheel
+    const wheels = document.querySelectorAll('.wheel');
+    
+    // Устанавливаем начальный класс open для первого элемента
+    document.querySelector('.moisture__view[data-card="1"]').classList.add('open');
+
+    // Добавляем обработчик события клика для каждого элемента
+    wheels.forEach(wheel => {
+        wheel.addEventListener('click', function() {
+            // Убираем класс open у всех элементов с классом moisture__view
+            document.querySelectorAll('.moisture__view').forEach(card => {
+                card.classList.remove('open');
+            });
+
+            // Получаем номер колеса
+            const wheelNumber = this.getAttribute('data-wheel');
+
+            // Добавляем класс open к соответствующему элементу moisture__view
+            document.querySelector(`.moisture__view[data-card="${wheelNumber}"]`).classList.add('open');
+        });
+    });
+});
